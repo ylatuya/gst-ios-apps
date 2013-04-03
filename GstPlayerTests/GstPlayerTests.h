62cfe3fb4f047c876a68305e03d7d5d45efaf253
Thiago Santos <thiago.sousa.santos@collabora.com>
2013-04-03 15:04:50 -0300

    gstplayer: Initial Commit

---
+++ GstPlayerTests/GstPlayerTests.h
@@ -0,0 +1,13 @@
+//
+//  GstPlayerTests.h
+//  GstPlayerTests
+//
+//  Created by Thiago Sousa Santos on 4/3/13.
+//  Copyright (c) 2013 Thiago Sousa Santos. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GstPlayerTests : SenTestCase
+
+@end
---
+++ GstPlayerTests/GstPlayerTests.m
@@ -0,0 +1,32 @@
+//
+//  GstPlayerTests.m
+//  GstPlayerTests
+//
+//  Created by Thiago Sousa Santos on 4/3/13.
+//  Copyright (c) 2013 Thiago Sousa Santos. All rights reserved.
+//
+
+#import "GstPlayerTests.h"
+
+@implementation GstPlayerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GstPlayerTests");
+}
+
+@end
